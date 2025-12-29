4d2dbbb06abfa459009fcc23e75d8862d459df29
Asad <asadmohd9411@gmail.com>
2025-12-29 12:21:26 +0530

    Replace ShiftScaleRotate with Affine transformation

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,6 +1,6 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-
+import cv2
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD  = (0.229, 0.224, 0.225)
@@ -9,14 +9,22 @@ IMAGENET_STD  = (0.229, 0.224, 0.225)
 def build_transforms(image_size: int, use_clahe: bool):
     pre = []
     if use_clahe:
-        # Works best on grayscale. If input is 3-channel replicated grayscale, CLAHE still helps.
         pre.append(A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=0.5))
 
     train_tf = A.Compose(
         pre + [
             A.Resize(image_size, image_size),
             A.HorizontalFlip(p=0.5),
-            A.ShiftScaleRotate(shift_limit=0.08, scale_limit=0.15, rotate_limit=15, border_mode=0, p=0.7),
+            A.Affine(
+                translate_percent=0.08,
+                scale=(0.85, 1.15),
+                rotate=(-15, 15),
+                shear=(-8, 8),
+                interpolation=cv2.INTER_LINEAR,
+                mode=cv2.BORDER_CONSTANT,
+                cval=0,
+                p=0.7,
+            ),
             A.RandomBrightnessContrast(p=0.3),
             A.GaussNoise(p=0.2),
             A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
